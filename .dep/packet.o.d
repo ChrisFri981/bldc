6be49ad30e5c172a47ced5292571af755293b2ca
Christian Fritz <christian.fritz@tum.de>
2017-11-20 18:23:15 +0100

    better usage

---
+++ .dep/packet.o.d
@@ -0,0 +1,70 @@
+build/obj/packet.o: packet.c \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\string.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\_ansi.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\newlib.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\_newlib_version.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\config.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\machine\ieeefp.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\features.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\reent.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\_ansi.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\lib\gcc\arm-none-eabi\6.3.1\include\stddef.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\_types.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\machine\_types.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\machine\_default_types.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\lock.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\cdefs.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\xlocale.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\string.h \
+ packet.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\lib\gcc\arm-none-eabi\6.3.1\include\stdint.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\stdint.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\_intsup.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\_stdint.h \
+ crc.h
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\string.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\_ansi.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\newlib.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\_newlib_version.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\config.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\machine\ieeefp.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\features.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\reent.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\_ansi.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\lib\gcc\arm-none-eabi\6.3.1\include\stddef.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\_types.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\machine\_types.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\machine\_default_types.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\lock.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\cdefs.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\xlocale.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\string.h:
+
+packet.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\lib\gcc\arm-none-eabi\6.3.1\include\stdint.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\stdint.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\_intsup.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\6\ 2017-q2-update\arm-none-eabi\include\sys\_stdint.h:
+
+crc.h:
